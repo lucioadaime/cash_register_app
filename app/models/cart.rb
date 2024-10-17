99ad629d729e7b93b0df08834374e26a512db3d7
Lucio Adaime <ladaime@miwendo.com>
2024-10-17 11:00:03 +0200

    Have discount logic be separate

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -41,53 +41,17 @@ class Cart
     end
   end
 
-  def load_discounts
-    file_path = File.expand_path("../../data/discounts.json", __dir__)
-    JSON.parse(File.read(file_path))
-  end
 
   def total_price
     total = 0.0
     # Decide whether a discount applies
     @items.each do |product_code, item|
-      if @discounts[product_code]
-        # If it applies we calculate the new value
-        total += apply_discount(item[:product], item[:quantity], @discounts[product_code])
-        # puts "Discount Applied!"
-      else
-        # Otherwise we calculate total regularly
-        total += item[:product].price * item[:quantity]
-      end
+        total += @discount_manager.calculate_discounted_price(item[:product], item[:quantity])
     end
     @items.sum { |product, info| info[:product].price * info[:quantity] }  # Calculate total price
 
     total
   end
-def apply_discount(product, quantity,  discount)
-  case discount["type"]
-  # We apply the appropriate discount
-  when "BOGO"
-    items_bogo = quantity/2
-    (quantity - items_bogo) * product.price
-
-  when "bulk price drop"
-    if quantity >= discount["min_quantity"]
-      quantity * discount["new_price"]
-    else
-      quantity * product.price
-    end
-  when "bulk drop by percentage"
-    if quantity >= discount["min_quantity"]
-      (quantity * discount["%_price"] *  product.price * 100).ceil/100.0 # Rounding up the number to two decimal digits
-    else
-      quantity * product.price
-    end
-  else
-    quantity * product.price
-  end
-end
-
-
 
   def empty_cart
     @items.each do |product, info|
